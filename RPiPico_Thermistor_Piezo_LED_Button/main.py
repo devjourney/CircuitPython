633f45f7d28b3fbb11103e57e06e62a1aa86f243
Kevin Hazzard <devjourney@live.com>
2023-03-11 00:53:39 -0500

    Added temperature smoothing

--- RPiPico_Thermistor_Piezo_LED_Button/main.py
+++ RPiPico_Thermistor_Piezo_LED_Button/main.py
@@ -23,14 +23,31 @@ button = digitalio.DigitalInOut(board.GP5)
 button.switch_to_input(pull=digitalio.Pull.DOWN)
 buzzer = pwmio.PWMOut(board.GP16, frequency=680, duty_cycle=0, variable_frequency=True)
 last_press_state = False
-last_temp_C = 0.0
+next_temp_time = time.monotonic()
+max_readings = 17
+readings_accumulator = thermistor.temperature
+readings = [readings_accumulator]
+readings_index = 0
+
+def smooth_temperature(temp):
+    global readings_index
+    global readings_accumulator
+    readings_index = (readings_index + 1) % max_readings
+    if len(readings) < max_readings:
+        readings_accumulator = readings_accumulator + temp
+        readings.append(temp)
+    else:
+        readings_accumulator = readings_accumulator - \
+            readings[readings_index] + temp
+        readings[readings_index] = temp
+    return round(readings_accumulator / len(readings), 1)
 
 while True:
-    # current_temp = steinhart_temperature_C(10000 / (65535/thermistor.value - 1))
-    current_temp = round(thermistor.temperature, 1)
-    if abs(current_temp - last_temp_C) > 0.6:
-        last_temp_C = current_temp
-        print(f'Temp = {current_temp}℃ {current_temp * 1.8 + 32}℉')
+    temp = smooth_temperature(thermistor.temperature)
+    # temp = steinhart_temperature_C(10000 / (65535/thermistor.value - 1))
+    if time.monotonic() > next_temp_time:
+        next_temp_time = time.monotonic() + 3
+        print(f'Temp = {temp}℃ {temp * 1.8 + 32}℉')
     current_press_state = button.value
     if (current_press_state != last_press_state):
         # print(f'Changing button state from {last_press_state} to {current_press_state}')
